f8c165a7d5cbdf0ad76a8eb2f016cfc87a7a14ba
IvanXing <xingyifan10@qq.com>
2020-10-10 16:50:00 +0800

    创建路由

--- blog-1/app.js
+++ blog-1/app.js
@@ -1,15 +1,33 @@
+const handleBlogRouter = require('./src/router/blog');
+const handleUserRouter = require('./src/router/user');
+
 const serverHandle = (req, res) => {
   // 设置返回格式为json
   res.setHeader('Content-type', 'application/json');
 
-  const resData = {
-    name: 'Ivan',
-    site: 'SH',
-    // process是node提供的全局变量，根据yarn run dev or prd 识别cross-env设置不同设置环境
-    env: process.env.NODE_ENV  
+  // 处理 url
+  const url = req.url;
+  res.path = url.split('?')[0];
+
+  // 处理 blog 路由
+  const blogData = handleBlogRouter(req, res);
+  if (blogData) {
+    res.end(JSON.stringify(blogData));
+    return;
   }
 
-  res.end(JSON.stringify(resData));
+  // 处理 user 路由
+  const userData = handleUserRouter(req, res);
+  if (userData) {
+    res.end(JSON.stringify(userData));
+    return;
+  }
+
+  // 未命中路由，返回 404
+  res.writeHead(404, {"Content-type": "text/plain"});
+  res.write("404 Not Found");
+  res.end();
+
 };
 
 module.exports = serverHandle;
