01feb00146e88d888e3fe598c71225f8ed1fd96c
xingyifan <xingyifan10@qq.com>
2021-08-31 21:02:40 +0800

    cookie的登录验证&测试

--- blog-1/src/router/user.js
+++ blog-1/src/router/user.js
@@ -5,15 +5,31 @@ const handleUserRouter = (req, res) => {
     const method = req.method;  // GET POST
   
     // 登陆接口
-    if (method === 'POST' && res.path === '/api/user/login') {
-      const { username, password } = req.body;
+    if (method === 'GET' && res.path === '/api/user/login') {
+      // const { username, password } = req.body;
+      const { username, password } = req.query;
       const result = login(username, password);
       return result.then(data => {
         if (data.username) {
+          
+          // 存储cookie path=/是跟路由，所有路由生效，否则就是本url
+          res.setHeader('Set-Cookie', `username=${data.username}; path=/`)
+
           return new SuccessModel()
         } 
         return new ErrorModel('登录失败')
       })
+    }
+
+    // 登录验证测试
+    if (method === 'GET' && res.path === '/api/user/login-test') {
+      // 登陆过才有cookie
+      if (req.cookie.username) {
+        return Promise.resolve(new SuccessModel({
+          username: req.cookie.username
+        }))
+      } 
+      return Promise.resolve(new ErrorModel('尚未登录'))
     }
 }
 
